339c0f1e3b79aa724857ce169ac62a1369db57cd
Gilmarapolly01 <gilmarapolly@hotmail.com>
2024-08-20 01:37:48 -0300

    Desafio Classificador de nível de Herói

---
+++ src/Desavio de classificação de nível de Herói.js
@@ -0,0 +1,18 @@
+let nomeHero = "Mulher Maravilha";
+let xP = 100;
+
+if (xP < 1000) {
+    console.log(nomeHero + " Está no nivel " +" Ferro");
+} else if (xP >= 1000 && xP <= 2000) {
+    console.log(nomeHero + " Está no nivel " + " Bronze");
+} else if (xP > 2000 && xP <= 5000) {
+    console.log(nomeHero + " Está no nivel " + " Prata Ouro");
+} else if (xP > 5000 && xP <= 8000) {
+    console.log(nomeHero + " Está no nivel " + " Platina Diamante");
+} else if (xP > 8000 && xP <= 9000) {
+    console.log(nomeHero + " Está no nivel " + " Ascendente");
+} else if (xP > 9000 && xP <= 10000) {
+    console.log(nomeHero + " Está no nive l" + " Imortal");
+} else if (xP > 10001) {
+    console.log(nomeHero + " Está no nivel " + " Radiante");
+}
