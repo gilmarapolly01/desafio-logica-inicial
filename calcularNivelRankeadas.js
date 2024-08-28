9658334983d5d849aa1d3d48a1093ea100c0f54e
Gilmarapolly01 <gilmarapolly@hotmail.com>
2024-08-27 22:32:59 -0300

    Desafio Nº 2 calcularNivelRankeadas

---
+++ calcularNivelRankeadas.js
@@ -0,0 +1,29 @@
+let vitorias = 30
+let derrotas = 10
+let resultado = calcularNivelRankeadas (vitorias, derrotas)
+console.log(resultado)
+
+function calcularNivelRankeadas (vitorias, derrotas){
+    let saldoVitorias = vitorias - derrotas
+    let nivel = ""
+
+    if(saldoVitorias < 10){
+        nivel = "Ferro"
+    } else if (saldoVitorias <= 20){
+        nivel = "Broze"
+    }else if (saldoVitorias <= 50){
+        nivel = "Prata"
+    }else if (saldoVitorias <= 80){
+        nivel = "Ouro"
+    }else if (saldoVitorias <= 90){
+        nivel = "Diamante"
+    }else if (saldoVitorias <= 100){
+        nivel = "Lendário"
+    } else{
+        nivel = "Imortal"
+    }
+
+    return `O O Herói tem saldo de ${saldoVitorias} está no nível de ${nivel}`
+
+}
+
